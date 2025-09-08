1b19081394d6ff234f79ab2f4175d53109c7eed1
Alcantara18 <junioor_99@hotmail.com>
2025-09-08 22:39:58 +0000

    Desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,32 +13,58 @@ int main() {
   printf("Desafio Super Trunfo Países\n");
   printf("Tema 1 Cadastro das Cartas\n");
 
+ //Cartas
  char Carta1[30] = "Carta1";
  char Carta2[30] = "Carta2";
 
+ //Estado
  char estadoA[20] = "Ceara";
  char estadoB[20] = "Bahia";
 
+ //Codigo da Carta
  char codigoA[20] = "A01";
  char codigoB[20] = "B01";
 
+ //Cidade
  char cidadeA[20] = "Fortaleza";
  char cidadeB[20] = "Salvador";
 
- int populaçãoA = 12325000;
- int populaçãoB = 67480008;
+ //População
+ unsigned long int populaçãoA = 12325000;
+ unsigned long int populaçãoB = 67480008;
 
- float áreaA = 351.136;
- float áreaB = 531.197;
+ //Área
+ float areaA = 351.136;
+ float areaB = 531.197;
 
- float PIBA = 603.478;
- float PIBB = 806.897;
+ //PIB
+ double PIBA = 73294603.478;
+ double PIBB = 95659806.897;
 
+ //Pontos Turísticos
  int PTA = 13;
  int PTB = 19;
 
+ //Densidade Populacional
+ float DePOA;
+ float DePoB;
 
+ //PIB per Capita
+ float PPCA;
+ float PPCB;
+
+ //Super Poder
+ float SPA;
+ float SPB;
+
+ DePOA = populaçãoA / areaA;
+ DePoB = populaçãoB / areaB;
+
+ PPCA = PIBA / populaçãoA;
+ PPCB = PIBB / populaçãoB;
  
+ SPA = populaçãoA + areaA + PIBA + PTA + DePOA + PPCA;
+ SPB = populaçãoB + areaB + PIBB + PTB + DePoB + PPCB;
 
 
 
@@ -57,9 +83,12 @@ int main() {
     printf("Código: %s\n", codigoA);
     printf("Cidade: %s\n", cidadeA);
     printf("População: %u\n", populaçãoA);
-    printf("Área: %.3f km²\n", áreaA),
+    printf("Área: %.3f km²\n", areaA),
     printf("PIB: %.3f\n", PIBA);
     printf("Números de Pontos Turisticos: %d\n", PTA );
+    printf("Densidade Populacional: %.2f\n", DePOA);
+    printf("PIB per Capita: %.2f\n", PPCA);
+    printf("Super Poder: %.2f\n", SPA);
 
 
     //Carta B
@@ -69,10 +98,54 @@ int main() {
     printf("Código: %s\n", codigoB);
     printf("Cidade: %s\n", cidadeB);
     printf("População: %u\n", populaçãoB);
-    printf("Área: %.3f km²\n", áreaB),
+    printf("Área: %.3f km²\n", areaB),
     printf("PIB: %.3f\n", PIBB);
     printf("Números de Pontos Turisticos: %d\n", PTB );
+    printf("Densidade Populacional: %.2f\n", DePoB);
+    printf("PIB per Capita: %.2f\n", PPCB);
+    printf("Super Poder: %.2f\n", SPB);
 
+    if(populaçãoA > populaçãoB){
+      printf("População: Carta 1 Venceu!\n");
+    }else{
+      printf("População: Carta 2 Venceu!\n");
+    }
+
+    if (areaA > areaB){
+      printf("Área: Carta 1 Venceu!\n");
+    }else{
+      printf("Área: Carta 2 Venceu!\n");
+    }
+
+    if(PIBA > PIBB){
+      printf("PIB: Carta 1 Venceu!\n");
+    }else{
+      printf("PIB: Carta 2 Venceu!\n");
+    }
+
+    if(PTA > PTB){
+      printf("Pontos Turísticos: Carta 1 Venceu!\n");
+    }else{
+      printf("Pontos Turísticos: Carta 2 Venceu!\n");
+    }
+
+    if(DePOA > DePoB){
+      printf("Densidade Populacional: Carta 1 Venceu!\n");
+    }else{
+      printf("Densidade Populacional: Carta 2 Venceu!\n");
+    }
+
+    if(PPCA > PPCB){
+      printf("PIB per Capita: Carta 1 Venceu!\n");
+    }else{
+      printf("PIB per Capita: Carta 2 Venceu!\n");
+    }
+
+    if(SPA > SPB){
+      printf("Super Poder: Carta 1 Venceu!\n");
+    }else{
+      printf("Super Poder: Carta 2 Venceu!\n");
+    }
 
 
 
