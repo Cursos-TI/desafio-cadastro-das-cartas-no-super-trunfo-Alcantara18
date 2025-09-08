ce8a1edc721803402e7c7aad2fe68a5a8fa45859
Alcantara18 <junioor_99@hotmail.com>
2025-09-08 21:35:21 +0000

    Desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,8 +25,8 @@ int main() {
  char cidadeA[20] = "Fortaleza";
  char cidadeB[20] = "Salvador";
 
- unsigned int populaçãoA = 12325000;
- unsigned int populaçãoB = 67480008;
+ int populaçãoA = 12325000;
+ int populaçãoB = 67480008;
 
  float áreaA = 351.136;
  float áreaB = 531.197;
@@ -38,18 +38,15 @@ int main() {
  int PTB = 19;
 
 
+ 
+
+
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     //Carta A
-    
-
-     float densidadepopA  = (float) populaçãoA / áreaA;
-     float pibpercaA = (float) populaçãoA / PIBA;
-     float SuperPoderA = (float) populaçãoA + áreaA + PIBA + PTA + densidadepopA + pibpercaA;
-
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
@@ -63,16 +60,9 @@ int main() {
     printf("Área: %.3f km²\n", áreaA),
     printf("PIB: %.3f\n", PIBA);
     printf("Números de Pontos Turisticos: %d\n", PTA );
-    printf("Densidade Populacional: %f\n", densidadepopA);
-    printf("PIB per Capita: %f\n", pibpercaA);
-    printf("Super Poder: %f\n", SuperPoderA);
+
 
     //Carta B
-   
-
-     float densidadePopB  = (float) populaçãoB / áreaB;
-     float PibPerCaB = (float) populaçãoB / PIBB;
-     float SuperPoderB = (float) populaçãoB + áreaB + PIBB + PTB + densidadePopB + PibPerCaB;
     
     printf("***Carta B***\n");
     printf("Estado: %s\n", estadoB);
@@ -82,25 +72,7 @@ int main() {
     printf("Área: %.3f km²\n", áreaB),
     printf("PIB: %.3f\n", PIBB);
     printf("Números de Pontos Turisticos: %d\n", PTB );
-    printf("Densidade Populacional: %f\n", densidadePopB);
-    printf("PIB per Capita: %f\n", PibPerCaB);
-    printf("Super Poder: %f\n", SuperPoderB);
 
-    int resultadoP = populaçãoA > populaçãoB;
-    int resultadoA = áreaA > áreaB;
-    int resultadoPIB = PIBA > PIBB;
-    int resultadoPT = PTA > PTB;
-    int resultadoDP = densidadepopA > densidadePopB;
-    int resultadoPPC = pibpercaA > PibPerCaB;
-    int resultadoSP = SuperPoderA > SuperPoderB;
-
-    printf("População: Carta 2 venceu (%d)\n", resultadoP);
-    printf("Área: Carta 2 venceu (%d)\n", resultadoA);
-    printf("PIB: Carta 2 venceu (%d)\n", resultadoPIB);
-    printf("Pontos Turísticos: Carta 2 venceu (%d)\n", resultadoPT);
-    printf("Densidade Populacional: Carta 2 venceu (%d)\n", resultadoDP);
-    printf("PIB per Capita: Carta 2 venceu (%d)\n", resultadoPPC);
-    printf("Super Poder: Carta 2 venceu (%d)\n", resultadoSP);
 
 
 
